5d53456281a1b525b1cc114268c1f3db1b0105e9
usiel <824797605@qq.com>
2018-06-07 18:28:05 +0800

    补充基础类，图的表示

---
+++ src/graph/Graph.java
@@ -0,0 +1,59 @@
+package graph;
+
+import base.Bag;
+import base.In;
+
+public class Graph {
+
+	private final int V;
+	private int E;
+	private Bag<Integer>[] adj;
+	public Graph(int V) {
+		this.V=V;
+		this.E=0;
+		adj=(Bag<Integer>[])new Bag[V];
+		for(int v=0;v<V;v++) {
+			adj[v]=new Bag<Integer>();
+		}
+	}
+	
+	public Graph(In in) {
+		this(in.readInt());
+		int E=in.readInt();
+		for(int i=0;i<E;i++) {
+			int v=in.readInt();
+			int w=in.readInt();
+			addEdge(v,w);
+		}
+	}
+	
+	public int V() {
+		return V;
+	}
+	
+	public int E() {
+		return E;
+	}
+	
+	public void addEdge(int v,int w) {
+		adj[v].add(w);
+		adj[w].add(v);
+		E++;
+	}
+	
+	public Iterable<Integer> adj(int v){
+		return adj[v];
+	}
+	
+	public String toString() {
+		String s=V+"vertices,"+E+"edges\n";
+		for(int v=0;v<V;v++) {
+			s+=v+":";
+			for(int w:this.adj(v)) {
+				s+=w+" ";
+			}
+			s+="\n";
+		}
+		return s;
+	}
+}
